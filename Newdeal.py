e55445edce2d2074e2130740567e2216f1079daf
Lordyuyox <48773061+Lordyuyox@users.noreply.github.com>
2022-04-08 10:33:12 -0400

    Update Newdeal.py

--- Newdeal.py
+++ Newdeal.py
@@ -4,18 +4,11 @@
 
 # Graficador de datos del Survey SHINING
 
-import csv 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import plotly.express as px
 import pandas as pd
 import dash
-from dash import dcc
-from dash import html
-#from dash import dash_html_components as html
-from dash.dependencies import Input, Output
-
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import plotly.graph_objs as go
 #Functions
 
 def Jy_to_ergs(cont1, cont2):
@@ -35,6 +28,7 @@ def generate_table(dataframe, max_rows=30):
     ])
 
 
+
 app = dash.Dash(__name__)
 
 colors = {
@@ -44,168 +38,10 @@ colors = {
 
 df = pd.read_csv('resolvedmaps_fluxes - Table6_MRF(update).csv')
 
+
 fig1 = px.scatter(df, x='Luminosity(log)', y='NIII57/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=True, title='Luminosity vs. NIII57/FIR')
-fig2 = px.scatter(df, x='Luminosity(log)', y='OI63/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=False, title='Luminosity vs. OI63/FIR')
-fig3 = px.scatter(df, x='Luminosity(log)', y='OIII88/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=False, title='Luminosity vs. OIII88/FIR')
-fig4 = px.scatter(df, x='Luminosity(log)', y='NII122/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=False, title='Luminosity vs. NII122/FIR')
-fig5 = px.scatter(df, x='Luminosity(log)', y='OI145/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=False, title='Luminosity vs. OI145/FIR')
-fig6 = px.scatter(df, x='Luminosity(log)', y='CII158/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=False, title='Luminosity vs. CII158/FIR')
 
-fig1.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
 
-fig2.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-fig3.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-fig4.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-fig5.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-fig6.update_layout(
-    legend=dict(
-        x=0,
-        y=0,
-        traceorder="reversed",
-        title_font_family="Times New Roman",
-        font=dict(
-            family="Courier",
-            size=12,
-            color="black"
-        ),
-        bgcolor="LightSteelBlue",
-        bordercolor="Black",
-        borderwidth=2
-        ),
-    xaxis=dict(
-        type='log'
-        ),
-    yaxis=dict(
-        type='log'
-        ),
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
 
 app.layout = html.Div(children=[
     html.H1(children='SHINING Survey Data',
@@ -221,22 +57,74 @@ app.layout = html.Div(children=[
     ),
     generate_table(df),
 
-    html.Div(children=['''
+    html.Div(children='''
         The data used in the development of the paper: 
         SHINING, A Survey of Far Infrared Lines in Nearby Galaxies. I: Survey Description, Observational Trends, and Line Diagnostics &
         SHINING, A Survey of Far Infrared Lines in Nearby Galaxies. II: Line-Deficit Models, AGN impact, [CII]-SFR Scaling Relations, and Mass-Metallicity Relation in (U)LIRGS
-    ''',
-    ]),
-
+    '''
+    ),
+    dcc.Dropdown(
+        id="dropdown",
+        options=['NIII57/FIR', 'OI63/FIR', 'OIII88/FIR', 'NII122/FIR', 'OI145/FIR', 'CII158/FIR'],
+        value=['Luminosity(log)', 'Brightness (log)'],
+        multi=False
+    ),
 
     dcc.Graph(
         id='example-graph',
         figure=fig1
-        )
-    ]
+        ),
+    html.P('Filter by Distance in Mpc_'),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=600, step=1,
+        marks={0: '0', 600: '600'},
+        value=[0.0, 600]
+    )
+])
+
+
+
+
+
+@app.callback(
+    Output("example-graph","figure"),
+    Input("range-slider", "value"),
 )
-    
-    
+
+
+def update_bar_chart(slider_range):
+    df = pd.read_csv('resolvedmaps_fluxes - Table6_MRF(update).csv')
+    low, high = slider_range
+    mask = (df['DL[Mpc]'] > low) & (df['DL[Mpc]'] < high)
+    fig1 = px.scatter(df[mask], x='Luminosity(log)', y='NIII57/FIR', color= 'L_{CO}(log)', symbol= 'Optical_class', hover_name= 'Name', hover_data = ['Brightness (log)', 'DL[Mpc]'], size = 'Redshift', size_max= 100, log_x=True, log_y=True, title='Luminosity vs. NIII57/FIR')
+    fig1.update_layout(
+    legend=dict(
+        x=0,
+        y=0,
+        traceorder="reversed",
+        title_font_family="Times New Roman",
+        font=dict(
+            family="Courier",
+            size=12,
+            color="black"
+        ),
+        bgcolor="LightSteelBlue",
+        bordercolor="Black",
+        borderwidth=2
+        ),
+    xaxis=dict(
+        type='log'
+        ),
+    yaxis=dict(
+        type='log'
+        ),
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+    )
+    return fig1
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
